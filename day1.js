71a7c04172524905b48c322079be5163618feb4b
Om Aute <om.aute2002@gmail.com>
2024-07-13 13:07:38 +0530

    day1 commit

---
+++ day1.js
@@ -0,0 +1,13 @@
+var num1 = 2;
+let str = "om";
+
+console.log(num1);
+console.log(str);
+
+const val = true;
+console.log(val);
+console.log(typeof(val));
+
+let val2 = 2;
+val2 = 45;
+console.log(val2);
